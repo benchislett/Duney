2d8bc1ab967dee0b78a40ba86aa8f7b7d799c64a
Benjamin Chislett <chislett.ben@gmail.com>
2024-02-08 23:00:35 +0100

    Simple PNG example

--- main.cu
+++ main.cu
@@ -1,3 +1,15 @@
+#include "lodepng.h"
+
+#include <iostream>
+#include <vector>
+
+void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
+    unsigned error = lodepng::encode(filename, image, width, height);
+    if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+}
+
 int main() {
+    std::vector<unsigned char> data = {255, 0, 255, 255, 0, 0, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255};
+    encodeOneStep("tmp.png", data, 2, 2);
     return 0;
 }
