cd45dea46c0d9e168b3ad5551ee3644f98ef173e
Benjamin Chislett <chislett.ben@gmail.com>
2024-02-11 18:22:17 +0100

    Complete simulation

--- main.cu
+++ main.cu
@@ -129,9 +129,65 @@ void serialize(const char *filename, const Grid<unsigned int>& data)
         std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 }
 
+bool climb_next(Grid<unsigned int>& hmap, int row, int col, int& out_row, int& out_col) {
+    int current = hmap.at_wrap(row, col);
+    int vals[6] = {-1, 1, 0, 0, -1, 1};
+    for (int i = 0; i < 4; i++) {
+        int dx = vals[i + 0];
+        int dy = vals[i + 2];
+
+        int next = hmap.at_wrap(row + dy, col + dx);
+        if (next - current > 2) {
+            out_row = row + dy;
+            out_col = col + dx;
+            return true;
+        }
+    }
+    out_row = row;
+    out_col = col;
+    return false;
+}
+
+void climb(Grid<unsigned int>& hmap, int row, int col, int& out_row, int& out_col) {
+    while (climb_next(hmap, row, col, out_row, out_col)) {
+        row = out_row;
+        col = out_col;
+    }
+    out_row = hmap.wrap_row(out_row);
+    out_col = hmap.wrap_row(out_col);
+}
+
+bool descend_next(Grid<unsigned int>& hmap, int row, int col, int& out_row, int& out_col) {
+    int current = hmap.at_wrap(row, col);
+    int vals[6] = {-1, 1, 0, 0, -1, 1};
+    for (int i = 0; i < 4; i++) {
+        int dx = vals[i + 0];
+        int dy = vals[i + 2];
+
+        int next = hmap.at_wrap(row + dy, col + dx);
+        if (next - current < -2) {
+            out_row = row + dy;
+            out_col = col + dx;
+            return true;
+        }
+    }
+    out_row = row;
+    out_col = col;
+    return false;
+}
+
+void descend(Grid<unsigned int>& hmap, int row, int col, int& out_row, int& out_col) {
+    while (descend_next(hmap, row, col, out_row, out_col)) {
+        row = out_row;
+        col = out_col;
+    }
+    out_row = hmap.wrap_row(out_row);
+    out_col = hmap.wrap_row(out_col);
+}
+
 int main()
 {
-    int length = 512;
+    int length = 256;
     int width = length, height = length;
     Grid<unsigned int> hmap(width, height);
 
@@ -139,13 +195,16 @@ int main()
     int max_height = 3;
     std::random_device rd;
     std::mt19937 gen(0);
-    std::uniform_int_distribution<> distr(min_height, max_height); // define the (inclusive) range
+    std::uniform_int_distribution<> distr_heightmap(min_height, max_height);
+    std::uniform_int_distribution<> distr_rows(0, height - 1);
+    std::uniform_int_distribution<> distr_cols(0, width - 1);
+    std::uniform_real_distribution<float> distr_01(0.0f, 1.0f);
 
     {
         ScopedTimer _timer("Height Map");
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                hmap.at(row, col) = distr(gen);
+                hmap.at(row, col) = distr_heightmap(gen);
             }
         }
     }
@@ -183,6 +242,76 @@ int main()
 
                 shadow.at(row, col) = in_shadow ? 1 : 0;
             }
+        }
+    }
+
+    {
+        ScopedTimer _timer("Update");
+        for (int epoch = 0; epoch < 1000; epoch++) {
+
+            for (int iter = 0; iter < width * height; iter++) {
+                int row = distr_rows(gen);
+                int col = distr_cols(gen);
+                if (hmap.at(row, col) == 0) continue;
+                if (shadow.at(row, col) == 1) continue;
+
+                // remove the grain from this position
+                int erode_row, erode_col;
+                climb(hmap, row, col, erode_row, erode_col);
+                hmap.at(erode_row, erode_col)--;
+
+                // hop downwind
+                int deposit_col = erode_col;
+                while (true) {
+                    deposit_col = hmap.wrap_col(deposit_col + 1); // saltation length
+                    if (shadow.at(erode_row, deposit_col) == 1) {
+                        descend(hmap, erode_row, deposit_col, erode_row, deposit_col);
+                        hmap.at(erode_row, deposit_col)++;
+                        break;
+                    } else {
+                        bool has_sand = hmap.at(erode_row, deposit_col) > 0;
+                        if (distr_01(gen) < (has_sand ? 0.6f : 0.4f)) {
+                            descend(hmap, erode_row, deposit_col, erode_row, deposit_col);
+                            hmap.at(erode_row, deposit_col)++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                bool in_shadow = false;
+
+                float curr_height = (float) hmap.at(row, col);
+                float curr_x = (float) col + 0.5f; // check from center for 50% coverage
+
+                for (int prev_col = col - 1; prev_col >= 0; prev_col--) {
+                    float prev_height = (float) hmap.at(row, prev_col);
+                    float prev_x = (float) prev_col + 1.0f; // check the rightmost point as it casts the furthest shadow
+
+                    float rise = (prev_height - curr_height) / 3.0f; // slabs are 1/3 units tall
+                    if (rise <= 0) continue;
+                    float run = curr_x - prev_x;
+                    float ratio = rise / run;
+                    if (ratio > 0.26795) {
+                        in_shadow = true;
+                        break;
+                    }
+
+                    // early stoppage check based on global max height
+                    ratio = ((float)max_height - curr_height) / (3.0f * run);
+                    if (ratio < 0.26795) {
+                        break;
+                    }
+                }
+
+                shadow.at(row, col) = in_shadow ? 1 : 0;
+            }
+        }
+
+        std::string name = "outputs/" + std::to_string(epoch) + ".png";
+        serialize(name.c_str(), hmap);
         }
     }
     
